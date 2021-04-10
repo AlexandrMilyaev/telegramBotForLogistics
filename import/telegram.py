52297e3ba37e1187e4e845857bf6de48d6c57873
milyaev.aleksandr <shyryp2009@gmal.com>
2021-04-11 02:15:26 +0300

    Бета версия бота. Добавил процедуру авторизации. Добавил метод создания маршрута, пока без складов

--- import/telegram.py
+++ import/telegram.py
@@ -1,12 +1,14 @@
 from aiogram.utils.helper import Helper, HelperMode, ListItem
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from wialon import Wialon, WialonError
+import time
 
 comands_types = {
-    "/start": "Приветствие",
+    "/start": "Процедура авторизации",
     "/help": "Описание комант бота",
-    "/get_orders": "Получить список всех доступных заявок",
-    "/start_route": "Начало маршрута"
+    "/add_orders": "Добавить заявку в созданый маршрут",
+    "/start_route": "Создать новый маршрут",
+    "/test": "Это просто для теста"
 }
 
 
@@ -16,11 +18,17 @@ class States(StatesGroup):
     STATE_GET_ORDERS = State()
     STATE_CREATE_ROUTS = State()
 
+    STATE_GET_NUMBER = State()
+
 
 class Orders(Wialon):
+    orders_for_route = dict()
+    orders_list = dict()
     raw_data = None
     data_by_tags = None
     orders = None
+    user_id = None
+    itemIds = 22403020
 
     def __init__(self, wialon_object, **extra_params):
         super().__init__(**extra_params)
@@ -73,18 +81,171 @@ class Orders(Wialon):
                     if name['f'] == 32:
                         if name['p']['tags'] == [] or type(name['p']['tags']) == str:
                             tags_value = tags_key.get('No_tags')
-                            tags_value = list(tags_value)
-                            tags_value.append(name['n'])
+                            tags_value = dict(tags_value)
+                            tags_value.update({name['id']: name['n']})
                             tags_key['No_tags'] = tags_value
+                            self.orders_list.update({name['id']: name['n']})
                         else:
                             for tags in name['p']['tags']:
                                 if tags_key.get(tags) is None:
-                                    tags_value = list(tags_value)
-                                    tags_key.update({tags: tags_value})
+                                    tags_key.update({tags: ''})
+                                    tags_key[tags] = {name['id']: name['n']}
+                                    self.orders_list.update({name['id']: name['n']})
                                 else:
                                     tags_value = tags_key.get(tags)
-                                    tags_value = list(tags_value)
-                                    tags_value.append(name['n'])
+                                    tags_value.update({name['id']: name['n']})
                                     tags_key[tags] = tags_value
-            self.data_by_tags = tags_key
+                                    self.orders_list.update({name['id']: name['n']})
+
+        self.data_by_tags = tags_key
         return self.orders
+
+    def craete_route(self, orders_id: list, driver: int):
+        gis = {
+            "provider": 1,  # 0-нет, 1-gurtam, 2-google
+            "addPoints": 1,  # 0-не возвращать трек, 1-вернуть трек
+            "speed": 50  # скорость для оптимизации
+        }
+        params = {
+            "itemId": self.itemIds,
+            "orders": orders_id,
+            "units": [driver],
+            "warehouses": [],
+            "criterions": {},
+            "flags": 3,
+            "gis": gis
+        }
+        try:
+            response = self.wialon_object.call('order_optimize', params)
+        except WialonError as e:
+            print(e.args)
+            res = self.wialon_object.token_login(token=self.token)
+            self.wialon_object.sid = res['eid']
+            response = self.wialon_object.call('order_optimize', params)
+        self.get_orders()
+        route_id = int(time.time())
+        order_list = list()
+        try:
+            for keys, data in response.items():
+                if keys == 'details':
+                    pass
+                elif keys == 'success':
+                    pass
+                elif keys == 'summary':
+                    pass
+                else:
+                    vt = route_id
+                    i = 0
+                    for _ in data['orders']:
+                        number = _['id']
+                        vt += _['tm']
+                        data_orders = self.orders[0]['orders'][f'{orders_id[number]}']
+                        data_orders['f'] = 1
+                        data_orders['p']['r'] = {
+                            "id": route_id,  # id маршрута
+                            "i": i,  # порядковый номер (0..)
+                            "m": _['ml'],  # пробег с предыдущей точки по плану, м
+                            "t": _['tm'],  # время с предыдущей точки по плану, сек
+                            "vt": vt,  # время посещения по плану, UNIX_TIME
+                            "ndt": 300  # время, за которое должно прийти уведомление, с
+                        }
+                        data_orders['u'] = keys
+                        data_orders['rp'] = _['p']
+                        data_orders['uid'] = 0
+                        data_orders['id'] = 0
+                        data_orders['st'] = 0
+                        time_modul = route_id % 86400
+                        time_modul = route_id - time_modul + time.altzone
+                        data_orders['tt'] = time_modul + 86400
+                        data_orders['tf'] = time_modul
+                        data_orders['callMode'] = 'create'
+
+                        i += 1
+                        order_list.append(data_orders)
+        except WialonError as e:
+            print(e.args)
+
+        params = {
+            "itemId": self.itemIds,
+            "orders": order_list,
+            "routeId": route_id,
+            "callMode": 'create'
+        }
+        response = self.wialon_object.call('order_route_update', params)
+        return response
+
+    def get_driver(self, phone_number: str):
+        '''
+
+        :param phone_number: номер телефона , должен совпасть с номером водителя в Wialon
+        :return: если номер телефона совпал возврашаем id назначеного обьекта
+                если обьект не назначен возвращаеться 0
+                если номер телефона не совпал возвращаем None
+        '''
+        spec = {
+            "itemsType": "avl_resource",
+            "propType": "propitemname",
+            "propName": "drivers",
+            "propValueMask": "*",
+            "sortType": "drivers"
+        }
+        params = {
+            "spec": spec,
+            "force": 1,
+            "flags": 256,
+            "from": 0,
+            "to": 0
+        }
+        try:
+            response = self.wialon_object.call('core_search_items', params)
+
+        except WialonError as e:
+            res = self.wialon_object.token_login(token=self.token)
+            self.wialon_object.sid = res['eid']
+            response = self.wialon_object.call('core_search_items', params)
+
+        driver = response['items'][0]['drvrs']
+        for _ in driver.values():
+            if _['p'][-10:] != '':
+                number = _['p'][-10:]
+                if number == phone_number[-10:]:
+                    return _['bu']
+        return None
+
+
+'''
+        try:
+            spec = {
+                "itemsType": "avl_unit",
+                "propType": "property",
+                "propName": "sys_name",
+                "propValueMask": "*",
+                "sortType": "sys_name"
+            }
+            params = {
+                "spec": spec,
+                "force": 1,
+                "flags": 1,
+                "from": 0,
+                "to": 0
+            }
+            response = self.wialon_object.call('core_search_items', params)
+            return response
+        except WialonError as e:
+            spec = {
+                "itemsType": "avl_unit"
+            }
+            params = {
+                "spec": spec,
+                "force": 1,
+                "flags": 1,
+                "from": 0,
+                "to": 0
+            }
+            res = self.wialon_object.token_login(token=self.token)
+            self.wialon_object.sid = res['eid']
+            response = self.wialon_object.call('core_search_items', params)
+            print(e.args)
+            return response
+
+'''
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import states as states
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -10,8 +11,12 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import FSMContext
 
+import sqlite3 as sql
+
 import logging
 import config
+
+import pandas as pd
 
 
 def loyaut_keyboard_tags():
@@ -21,54 +26,141 @@ def loyaut_keyboard_tags():
     return inline_kb_full
 
 
+con = sql.connect('user.db')
+cur = con.cursor()
+'''
+with con:
+    con.execute("""
+        CREATE TABLE USER (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            name TEXT
+        );
+    """)
+'''
+
 bot = Bot(token=config.telegram_token)
-dp = Dispatcher(bot, storage=MemoryStorage())
+storege = MemoryStorage()
+dp = Dispatcher(bot, storage=storege)
 dp.middleware.setup(LoggingMiddleware())
-
-
 
 wialon_api = Wialon()
 result = wialon_api.token_login(token=config.logistics_token)
 wialon_api.sid = result['eid']
-orders = Orders(wialon_object=wialon_api, token=config.logistics_token)
+orders = Orders(wialon_object=wialon_api, token=config.logistics_token, itemId=22403020)
 logging.basicConfig(level=logging.INFO)
+orders_list = list()
+# print(user)
 
+df = pd.DataFrame({
+    'phone_number',
+    'first_name',
+    'last_name',
+    'user_id'
+})
+
+with open('import/contacts.txt', 'r', encoding='utf-8') as g:
+    pass
 
 
 @dp.message_handler(commands="start")
-async def cmd_start(message: types.Message):
-    await message.reply("Привет!\nЯ создан для того,\nчто бы ты в суботу мог работать!")
+async def cmd_test(message: types.Message):
+    inline_kb_full = types.ReplyKeyboardMarkup(row_width=2)
+    inline_kb_full.add(types.KeyboardButton('Отправить свой контакт ☎️', request_contact=True))
+    await States.STATE_GET_NUMBER.set()
+    await message.answer("Отправьте свой номер телефона для идентификации",
+                         reply_markup=inline_kb_full)
+
+
+@dp.message_handler(state=States.STATE_GET_NUMBER, content_types=types.ContentTypes.CONTACT)
+async def get_number(message: types.Message, state: FSMContext):
+    df = pd.read_csv('user.csv', delimiter=',')
+    phone = df['phone_number'].tolist()
+    try:
+        print(phone.index(int(dict(message.contact)['phone_number'])))
+        await message.answer('Ваш номер уже присутствует в списке авторизированиых номеров.\n'
+                             'Отправьте команду /help для получения информации'
+                             ' по доступным командам.')
+    except ValueError:
+        driver = orders.get_driver(dict(message.contact)['phone_number'])
+        if driver is None:
+            await message.answer('Извените, вы отсутствуете в списке разрешонных пользователей')
+        else:
+            list_pandas = [dict(message.contact)]
+            df = df.append(list_pandas)
+            df.to_csv('user.csv', index=False)
+            if driver == 0:
+                await message.answer('Авторизация прошла успешно,'
+                                     ' но за вами не закреплен автомобиль.\n'
+                                     ' Обратитесь к диспетчеру для назначения на автомобиль.')
+            else:
+                await message.answer('Авторизация прошла успешно.\n'
+                                     'Отпраьте команду /help для получения информации'
+                                     ' по доступным командам.')
+
+    finally:
+        phone.clear()
+        await state.finish()
 
 
 @dp.message_handler(commands="help")
 async def cmd_help(message: types.Message):
-    msg = "Я на стадии разработки,\nно вот что я умею уже сейчас:\n"
-    for cmd, text in comands_types.items():
-        msg += cmd + ' - ' + text + '\n'
-    await message.answer(msg)
+    user_id = message.from_user.id
+    df = pd.read_csv('user.csv', delimiter=',')
+    df_user_id = df['user_id'].tolist()
+    if user_id in df_user_id:
+        msg = "Я на стадии разработки,\nно вот что я умею уже сейчас:\n"
+        for cmd, text in comands_types.items():
+            msg += cmd + ' - ' + text + '\n'
+        await message.answer(msg)
+    else:
+        await message.answer('Мы не знакомы.\n'
+                             'Пройдите авторизацию, отправив команду /start')
 
 
-@dp.message_handler(state='*', commands="get_orders")
-async def cmd_get_orders(message: types.Message, state: FSMContext):
-    orders.get_orders()
-    keyboard = loyaut_keyboard_tags()
-    await state.update_data(tags=orders.data_by_tags.keys())
-    await States.STATE_GET_TAG.set()
-    await message.reply('Выбирите тег', reply_markup=keyboard)
+@dp.message_handler(commands="test")
+async def cmd_start(message: types.Message):
+    user_id = message.from_user.id
+    df = pd.read_csv('user.csv', delimiter=',')
+    df_user_id = df['user_id'].tolist()
+    if user_id in df_user_id:
+        await message.answer("Привет!\nЯ создан для того,\nчто бы ты в суботу мог работать!")
+    else:
+        await message.answer('Мы не знакомы.\n'
+                             'Пройдите авторизацию, отправив команду /start')
+
+@dp.message_handler(commands="start_route")
+async def cmd_start_route(message: types.Message, state: FSMContext):
+    user_id = message.from_user.id
+    df = pd.read_csv('user.csv', delimiter=',')
+    df_user_id = df['user_id'].tolist()
+    if user_id in df_user_id:
+        orders.get_orders()
+        keyboard = loyaut_keyboard_tags()
+        await state.update_data(tags=orders.data_by_tags.keys())
+        await States.STATE_GET_TAG.set()
+        await message.answer('Выбирите тег', reply_markup=keyboard)
+        print(message.from_user.id)
+    else:
+        await message.answer('Мы не знакомы.\n'
+                         'Пройдите авторизацию, отправив команду /start')
 
 
 @dp.message_handler(state=States.STATE_GET_TAG, content_types=types.ContentTypes.TEXT)
 async def get_tag(message: types.Message, state: FSMContext):
     if message.text in orders.data_by_tags.keys():
         keyboard = types.ReplyKeyboardMarkup(row_width=2)
-        for order in orders.data_by_tags[message.text]:
-            keyboard.add(types.KeyboardButton(f'{order}'))
+        keyboard.add(types.KeyboardButton('Далее ->'))
+        for tag, order in orders.data_by_tags[message.text].items():
+            keyboard.add(types.KeyboardButton(f'{tag}: {order}'))
+
         keyboard.add(types.KeyboardButton('Назад <-'))
-        await message.reply('Выбирите нужные заявки. Для выбора другого тега нажмите "Назад"',
-                            reply_markup=keyboard)
+        await state.update_data(tag_selected=message.text.casefold())
+        await message.answer('Выбирите нужные заявки. Для выбора другого тега нажмите "Назад"',
+                             reply_markup=keyboard)
         await States.STATE_GET_ORDERS.set()
     else:
-        await message.reply('Нет такого тега')
+        await message.answer('Нет такого тега')
 
 
 @dp.message_handler(state=States.STATE_GET_ORDERS, content_types=types.ContentTypes.TEXT)
@@ -77,8 +169,46 @@ async def get_order(message: types.Message, state: FSMContext):
         keyboard = loyaut_keyboard_tags()
         # await state.update_data(tags=orders.data_by_tags.keys())
         await States.STATE_GET_TAG.set()
-        await message.reply('Выбирите тег', reply_markup=keyboard)
+        await message.answer('Выбирите тег', reply_markup=keyboard)
 
+    elif message.text[message.text.find(':') + 2:] in orders.orders_list.values():
+        try:
+            orders.orders_for_route.update({int(message.text[:message.text.find(':')]):
+                                                message.text[message.text.find(':') + 2:]})
+            orders_list.append(message.text)
+            print(orders_list)
+            await message.answer(f'Заявка "{message.text}" добавлена в список для состовления маршрута')
+        except Exception as e:
+            await message.answer('Некоректная заявка')
+
+    elif message.text == 'Далее ->':
+        await States.STATE_CREATE_ROUTS.set()
+        keyboard = types.ReplyKeyboardMarkup(row_width=2)
+        keyboard.add(types.KeyboardButton('Да'))
+        keyboard.add(types.KeyboardButton('Нет'))
+        await message.answer(f'Создать маршрут с заявками {orders_list}?', reply_markup=keyboard)
+
+
+@dp.message_handler(state=States.STATE_CREATE_ROUTS, content_types=types.ContentTypes.TEXT)
+async def create_route(message: types.Message, state: FSMContext):
+    if message.text == 'Да':
+        df = pd.read_csv('user.csv', delimiter=',')
+        user_id = df['user_id'].tolist()
+        phone = df['phone_number'].tolist()
+        pointer_user_id = user_id.index(message.from_user.id)
+        phone = phone[pointer_user_id]
+        driver = orders.get_driver(str(phone))
+        orders.craete_route(list(orders.orders_for_route.keys()), driver)
+        orders.orders_for_route.clear()
+        orders_list.clear()
+        await message.answer('Маршрут создан')
+        await state.finish()
+    elif message.text == 'Нет':
+        orders.orders_for_route.clear()
+        orders_list.clear()
+        await States.STATE_GET_COMMAND.set()
+        await message.answer('Отмена создания маршрута!')
+        await state.finish()
 
 
 if __name__ == '__main__':
